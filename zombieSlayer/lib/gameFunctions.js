5022c4ad2bc472918fc05863f18b86d7349fea26
Brance Boren <branceboren@gmail.com>
2015-11-14 06:13:10 -0700

    Converted controls to WASD instead of arrow keys

--- zombieSlayer/lib/gameFunctions.js
+++ zombieSlayer/lib/gameFunctions.js
@@ -6,34 +6,33 @@ Meteor.gameFunctions = {
   Controls(playerId, canvas) {
       window.addEventListener("keydown", function(e) {
        switch(e.keyCode) {
-         case 37:
+         case 65:
            Meteor.call('LEFTd', Session.get("currentPlayerId")); break;
-         case 38:
+         case 87:
            Meteor.call('UPd', Session.get("currentPlayerId")); break;
-         case 39:
+         case 68:
            Meteor.call('RIGHTd', Session.get("currentPlayerId")); break;
-         case 40:
+         case 83:
            Meteor.call('DOWNd', Session.get("currentPlayerId")); break;
        }
       });
       window.addEventListener("keyup", function(e) {
        switch(e.keyCode) {
-         case 37:
+         case 65:
            Meteor.call('LEFTu', Session.get("currentPlayerId")); break;
-         case 38:
+         case 87:
            Meteor.call('UPu', Session.get("currentPlayerId")); break;
-         case 39:
+         case 68:
            Meteor.call('RIGHTu', Session.get("currentPlayerId")); break;
-         case 40:
+         case 83:
            Meteor.call('DOWNu', Session.get("currentPlayerId")); break;
        }
       });
       //this.canvas.addEventListener('mousedown', this.mouseDownEvent.bind(this));
       canvas.addEventListener('mouseup', function(e){
-
-          let rect = canvas.getBoundingClientRect();
+        let rect = canvas.getBoundingClientRect();
         Meteor.call("mouseUp", Session.get("currentPlayerId"), Session.get("currentGame"), e.clientY - rect.top, e.clientX - rect.left);
       });
       //this.canvas.addEventListener('contextmenu', this.contextMenuEvent.bind(this));
-  }
+    }
 };
